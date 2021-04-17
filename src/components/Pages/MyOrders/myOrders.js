7368ff6331157ef6a0422506861dda041eec2f7d
bharath <sharmabharath1998@gmail.com>
2021-04-17 15:15:27 +0530

    my orders

--- src/components/Pages/MyOrders/myOrders.js
+++ src/components/Pages/MyOrders/myOrders.js
@@ -16,7 +16,7 @@ const MyOrders = (props) => {
 
     const user = useSelector((state) => state.user.customer);
   const packages = ['WASHING','DETAILING','SANITIZATION']
-  const [ myCar,setCar] = useState(user.cars[0].details)
+  const [ myCar,setCar] = useState(user.cars[0]?.details)
   const [myOrder,setMyOrder] = useState('WASHING')
 
   const filteredCars = user.cars.filter(
@@ -62,36 +62,40 @@ const MyOrders = (props) => {
   
  
   return (
-    <div className="myOrderMainContainer">
-      <div className="myOrders-picker-container">
-        <div className="myOrders-cars-picker">
-          {user.cars?.map((oneCar) => (
-            <div
-              onClick={() => setCar(oneCar.details)}
-              id="carPicker"
-              key={oneCar.id}
-              style={
-                oneCar.details === myCar
-                  ? {
-                      backgroundColor: "#03588C",
-                      color: "white",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }
-                  : {
-                      background: "none",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }
-              }
-            >
-              <p>{oneCar.details}</p>
+    <div>
+      {user.cars.length === 0 ? (
+        <h1 style={{height:'100vh'}}>No Cars Yet</h1>
+      ) : (
+        <div className="myOrderMainContainer">
+          <div className="myOrders-picker-container">
+            <div className="myOrders-cars-picker">
+              {user.cars?.map((oneCar) => (
+                <div
+                  onClick={() => setCar(oneCar.details)}
+                  id="carPicker"
+                  key={oneCar.id}
+                  style={
+                    oneCar.details === myCar
+                      ? {
+                          backgroundColor: "#03588C",
+                          color: "white",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }
+                      : {
+                          background: "none",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }
+                  }
+                >
+                  <p>{oneCar.details}</p>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        {/* <div className="myOrders-service-picker">
+            {/* <div className="myOrders-service-picker">
           {myCar.orders?.map((mypackage) => (
             <div
               style={
@@ -107,58 +111,64 @@ const MyOrders = (props) => {
             </div>
           ))}
         </div> */}
-      </div>
-      <div className="carCard">
-        <div key={myCar.id} className="oneOrderBox">
-          <div className="ActiveOrdersContainer">
-            <div className="leftOrder">
-              <AlternateSub
-                superdailyStatus={superdailyStatus}
-                doubleClickHandler={DoubleClickHandler}
-              />
-            </div>
-            <div className="rightOrder">
-              <div style={{ display: "flex" }}>
-                <p id="myOrdersTopic">Car Name</p>
-                <p>:</p>
-                <p>{myCar}</p>
-              </div>
-              {rightData.filteredOrders.length === 0 ? (
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <h2>NO ORDERS THIS DAY</h2>
+          </div>
+          <div className="carCard">
+            <div key={myCar.id} className="oneOrderBox">
+              <div className="ActiveOrdersContainer">
+                <div className="leftOrder">
+                  <AlternateSub
+                    superdailyStatus={superdailyStatus}
+                    doubleClickHandler={DoubleClickHandler}
+                  />
                 </div>
-              ) : (
-                <div>
-                  {rightData.filteredOrders.map((oneOrder) => (
-                    <div>
-                      <div style={{ display: "flex" }}>
-                        <p id="myOrdersTopic">Service</p>
-                        <p>:</p>
-                        <p>{oneOrder?.service}</p>
-                      </div>
-
-                      <div style={{ display: "flex" }}>
-                        <p id="myOrdersTopic">Package</p>
-                        <p>:</p>
-                        <p>{oneOrder?.serviceType === "One Time" ? 'ONE TIME' : filteredServices[1].package}</p>
-                      </div>
-                      <div style={{ display: "flex" }}>
-                        <p id="myOrdersTopic">Date</p>
-                        <p>:</p>
-                        <p>{moment(oneOrder?.start).toString().substr(0,11)}</p>
-                      </div>
+                <div className="rightOrder">
+                  <div style={{ display: "flex" }}>
+                    <p id="myOrdersTopic">Car Name</p>
+                    <p>:</p>
+                    <p>{myCar}</p>
+                  </div>
+                  {rightData.filteredOrders.length === 0 ? (
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <h2>NO ORDERS THIS DAY</h2>
                     </div>
-                  ))}
-                </div>
-              )}
+                  ) : (
+                    <div>
+                      {rightData.filteredOrders.map((oneOrder) => (
+                        <div>
+                          <div style={{ display: "flex" }}>
+                            <p id="myOrdersTopic">Service</p>
+                            <p>:</p>
+                            <p>{oneOrder?.service}</p>
+                          </div>
 
-              {/* {rightData.package === "ONE TIME" ? null : (
+                          <div style={{ display: "flex" }}>
+                            <p id="myOrdersTopic">Package</p>
+                            <p>:</p>
+                            <p>
+                              {oneOrder?.serviceType === "One Time"
+                                ? "ONE TIME"
+                                : filteredServices[0].package}
+                            </p>
+                          </div>
+                          <div style={{ display: "flex" }}>
+                            <p id="myOrdersTopic">Date</p>
+                            <p>:</p>
+                            <p>
+                              {moment(oneOrder?.start).toString().substr(0, 11)}
+                            </p>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  )}
+
+                  {/* {rightData.package === "ONE TIME" ? null : (
                           <div style={{ display: "flex" }}>
                             <p id="myOrdersTopic">Days Remaining</p>
                             <p>:</p>
@@ -173,42 +183,44 @@ const MyOrders = (props) => {
                             </p>
                           </div>
                         )} */}
-              <div id="event-indication-container">
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <div id="red-indication"></div>
-                  <p>Missed</p>
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <div id="green-indication"></div>
-                  <p>Done</p>
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <div id="gray-indication"></div>
-                  <p>Pending</p>
+                  <div id="event-indication-container">
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <div id="red-indication"></div>
+                      <p>Missed</p>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <div id="green-indication"></div>
+                      <p>Done</p>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <div id="gray-indication"></div>
+                      <p>Pending</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
